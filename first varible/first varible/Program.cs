92b4feee21287e6968eda35b7a9cc7bc3cc656bc
Salih12adil <salih12adil@gmail.com>
2017-06-22 23:52:42 +0300

    first c sharp_progrim
    this is my first  csharp progri

---
+++ first varible/first varible/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace first_varible
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 25;
+            Console.WriteLine("my variable ={0}", x);
+            Console.ReadKey();
+        }
+    }
+}
