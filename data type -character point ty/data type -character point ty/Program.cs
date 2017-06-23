ca2c5c5af9f5cb5f8da731c328f7c956418c3ad4
Salih12adil <salih12adil@gmail.com>
2017-06-23 07:28:43 +0300

    data types-char string boolean
    this is a program to explain these types in c3

---
+++ data type -character point ty/data type -character point ty/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace data_type__character_point_ty
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int charMin = char.MinValue;
+
+            int charMax = char.MaxValue;
+
+            char myvar = 'G';
+            int myvalue='G';
+            
+            Console.WriteLine("\nMin char ={0} \nMax char ={1} my \ncharacter={2} \nmy value number={3}"
+                , charMin, charMax, myvar, myvalue );
+
+
+            string mystring = "This is my varioble";
+            Console.WriteLine("\nmystring is {0}" , mystring );
+
+            bool myBool = true;
+            Console.WriteLine("\nmy bool value is ={0}", myBool);
+
+
+            Console.ReadKey();
+
+
+
+
+
+
+
+        }
+    }
+}
