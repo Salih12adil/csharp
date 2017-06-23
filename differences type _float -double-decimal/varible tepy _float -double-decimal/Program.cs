b65ce86aef9141f3999e79f25ea451eac2535e12
Salih12adil <salih12adil@gmail.com>
2017-06-23 05:30:18 +0300

    differences type _float -double-decimaldifferences type _float -double-decimal
    this is a program differences type _float -double-decimal

---
+++ differences type _float -double-decimal/varible tepy _float -double-decimal/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace varible_tepy__float__double_decimal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {  float flval = 1F / 3;
+            double dlval = 1D / 3;
+            decimal dcval = 1M / 3;
+
+            Console.WriteLine("\nfloat value={0} \ndouble value ={1} \ndecimal value ={2}",flval, dlval,dcval);
+            Console.ReadKey();
+        }
+        }
+    }
+            
+
+
+
+   
