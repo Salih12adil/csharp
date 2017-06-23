9c2d2cdb6c85f0c98bb0d5e15ddfb589796d7a65
Salih12adil <salih12adil@gmail.com>
2017-06-23 05:17:53 +0300

    data type - float point type
    this is a program to explain the flloat point type and differences among them

---
+++ data tepy-float point ty/data tepy-float point ty/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace data_tepy_float_point_ty
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float flMin = float.MinValue;
+            float flMax = float.MaxValue;
+            Console.WriteLine ("Minvalue ={0} Maxvalue={1}",flMin,flMax);
+            Console.ReadKey();
+
+
+
+
+            float flval = 1F / 3;
+            double dlval = 1D / 3;
+            decimal dcval = 1M / 3;
+
+            Console.WriteLine("flout value={0} double value ={1} decimal value ={2}",flval, dlval,dcval);
+            Console.ReadKey();
+        }
+    }
+}
